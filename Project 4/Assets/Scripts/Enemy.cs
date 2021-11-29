c40795de8cb88e340d4e0c7616c4cb601514c3fc
Hide Ziegler <hidez246@gmail.com>
2021-11-29 12:46:51 -0700

    added enemy

---
+++ Project 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Variables
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Enemy heads towards player
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+        // When enemy falls off, it's destroyed
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
